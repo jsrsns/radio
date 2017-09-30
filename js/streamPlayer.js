1cfd420f803e1eef25a098721f92818c272472d7
jsrsns <joesroosens@gmail.com>
2017-09-30 13:20:44 +0200

    Update to streamPlayer.js

--- js/streamPlayer.js
+++ js/streamPlayer.js
@@ -18,7 +18,7 @@ function togglePlay() {
     playPause.attributes.d.value = "M0 0h6v24H0zM12 0h6v24h-6z";
     player.play();
   } else {
-    playPause.attributes.d.value = "M18 12L0 24V0";
+    playPause.attributes.d.value = "M35.8 159.8c0-5.5 0-14.5 0-20V45.9c0-5.5 4-8 8.9-5.5l96.2 48c4.9 2.5 13 6.5 17.9 8.9l96.2 48c4.9 2.5 4.9 6.5 0 8.9l-96.2 48c-4.9 2.5-13 6.5-17.9 8.9l-96.2 48c-4.9 2.5-8.9 0-8.9-5.5V159.8z";
     player.pause();
   }
 }
