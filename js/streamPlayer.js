fc2d7767f619182ad6fec09e2631a8cb894d9de2
jsrsns <joesroosens@gmail.com>
2017-09-30 11:00:12 +0200

    Update to streamPlayer.js

--- js/streamPlayer.js
+++ js/streamPlayer.js
@@ -1,4 +1,6 @@
 function setOffline()
 {
   $('#streamStateLabel > span').text("OFFLINE");
+  $('#streamPlayer').css('background-color','#fb0e1f');
+  $('#streamPlayerControls').css('display','none');
 }
