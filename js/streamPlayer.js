aca33176617e0c9947c6fe62aec4cd7efde340ee
jsrsns <joesroosens@gmail.com>
2017-09-30 12:20:46 +0200

    Update to streamPlayer.js

--- js/streamPlayer.js
+++ js/streamPlayer.js
@@ -4,3 +4,24 @@ function setOffline()
   $('#streamPlayer').css('background-color','#fb0e1f');
   $('#streamPlayerControls').css('display','none');
 }
+var audioPlayer = document.querySelector('#streamPlayer');
+var PlayPause = audioPlayer.querySelector('#playPause');
+var playPauseBtn = audioPlayer.querySelector('.play-pause-btn');
+var loading = audioPlayer.querySelector('.loading');
+
+playpauseBtn.addEventListener('click',togglePlay);
+
+function togglePlay() {
+  if(player.paused) {
+    playPause.attributes.d.value ="M0 0h6v24H0zM12 0h6v24h-6z";
+    player.play();
+  } else {
+    playPause.attributes.d.value ="M18 12L0 24V0"
+    player.pause();
+  }
+}
+
+function makePlay() {
+  playpauseBtn.style.display = 'block';
+  loading.style.display = 'none';
+}
