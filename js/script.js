a61ed34aba5ee8ee43df224e19ab5f977d59e09e
jsrsns <joesroosens@gmail.com>
2017-10-03 19:59:27 +0200

    Update to script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+/* Page load in */
 {
 setTimeout(() => document.body.classList.add('render'), 60);
 };
+
+/* Scrolling */
+
+/* Archive player */
+$('.load-btn').click(fuction(){
+  archive = '<iframe src="'+$(this).attr('data-id') +'"></iframe>';
+})
