c9c6b84f6de6aaf6368cef1098c8c582a2396051
Joes Roosens <joesroosens@gmail.com>
2017-10-04 16:19:55 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,6 +12,8 @@ for (var i = 0; i < archivePlayer.length; i++) {
 
   archivePlayer[i].addEventListener('click', function() {
     var iframe = document.createElement('iframe');
+    iframe.setAttribute("width", "100%");
+    iframe.setAttribute("height", "120");
     iframe.setAttribute("frameborder", "0");
     iframe.setAttribute("src", "https://www.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=" + this.dataset.embed);
 
